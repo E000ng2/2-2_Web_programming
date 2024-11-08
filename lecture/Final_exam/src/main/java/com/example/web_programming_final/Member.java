6902e07dc9ed5395ef8617bc24ab83be9f92653f
Yeyoung Jo <whdpud2002@naver.com>
2024-11-08 09:28:58 +0900

    4th commit

---
+++ lecture/Final_exam/src/main/java/com/example/web_programming_final/Member.java
@@ -0,0 +1,12 @@
+package com.example.web_programming_final;
+
+public class Member {
+    public String mid; 
+   public String pw;
+    /* 필드 2개 */
+    public Member(String mid, String pw) { /* 생성자 */
+    this.mid = mid;
+    this.pw = pw;
+    }
+    
+}
---
+++ lecture/Final_exam/src/test/java/com/example/web_programming_final/WebProgrammingFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_programming_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProgrammingFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
